09fd25042c7b425228d0ec8cc4418b4cfe8dcfcd
Moriawe <moriawe22@gmail.com>
2022-08-03 14:07:49 +0200

    add pictures of kittens

--- pages/kittens.vue
+++ pages/kittens.vue
@@ -1,15 +1,91 @@
+<style>
+#love {
+  max-width: 300px;
+}
+</style>
+
 <template>
   <h2>Kattungar</h2>
+  <h3>Kakan ❤️ Nuutii</h3>
+  <img id="love" src="img/kakanonuttii.jpg" />
   <h3>D20's Kakkull</h3>
   <p>Födda 17/7 2022</p>
-  <h3>Kakan ❤️ Nuutii</h3>
+
   <a
     href="https://www.pawpeds.com/db/?a=p&ids=3:1521144;2:1468858&g=4&p=eur&o=elastic"
     target="_blank"
     >Stamtavla / Pedigree</a
   >
-  <h4>Baddaren</h4>
-  <h4>Brago the Barbarian</h4>
-  <h4>Singoalla the Bard</h4>
-  <h4>Ballerina</h4>
+
+  <div class="card-group">
+    <div class="card">
+      <img
+        src="img/kakkullen/Baddaren-Dice-2-small.jpg"
+        class="card-img-top"
+        alt="Baddaren"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Baddaren</h5>
+        <p class="card-text">
+          <small class="text-muted">Svart kille (EUR n)</small>
+        </p>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        src="img/kakkullen/Brago-Dice-2-small.jpg"
+        class="card-img-top"
+        alt="Brago"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Brago</h5>
+        <p class="card-text">
+          <small class="text-muted">Röd tjej (EUR d)</small>
+        </p>
+        <p class="card-text">
+          This card has supporting text below as a natural lead-in to additional
+          content.
+        </p>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        src="img/kakkullen/Ballerina-Dice-1-small.jpg"
+        class="card-img-top"
+        alt="Ballerina"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Ballerina</h5>
+        <p class="card-text">
+          <small class="text-muted">Svartsköldpadda smoke (EUR fs)</small>
+        </p>
+        <p class="card-text">
+          This card has supporting text below as a natural lead-in to additional
+          content.
+        </p>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        src="img/kakkullen/Singoalla-Dice-1-small.jpg"
+        class="card-img-top"
+        alt="Singoalla"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Singoalla</h5>
+        <p class="card-text">
+          <small class="text-muted">Svartsköldpadda smoke (EUR fs)</small>
+        </p>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This card has even longer content than the
+          first to show that equal height action.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
