98a7b54413dcb04dbd820f82289c9a81a9ebc926
Moriawe <moriawe22@gmail.com>
2022-08-26 18:28:13 +0200

    add made with comments

--- components/PageFoot.vue
+++ components/PageFoot.vue
@@ -13,5 +13,11 @@ footer {
     <footer>
       <p>© Jennie Lindahl 2022</p>
     </footer>
+    Made with Vue@3, Boostrap@5, Nuxt@3 /
+    <a
+      href="https://www.flaticon.com/free-icons/pedigree"
+      title="pedigree icons"
+      >Pedigree icons created by Freepik - Flaticon</a
+    >
   </div>
 </template>
